a18a8d55818a3336f4a8ceccd271038d866c1716
Talhashafique292 <talhashafique292@gmail.com>
2023-11-07 23:14:56 +0500

    Initial commit

---
+++ game.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let score = 0;
+async function startloop() {
+    let again;
+    do {
+        await guessNumber();
+        again = await inquirer.prompt([
+            {
+                name: "restart",
+                type: "list",
+                choices: ["yes", "No"],
+            }
+        ]);
+    } while (again.restart === "yes");
+}
+startloop();
+async function guessNumber() {
+    let guessNum = Math.floor(Math.random() * 10);
+    let tip;
+    if (guessNum % 2 == 0) {
+        tip = "Tip;: The number is even.";
+    }
+    else {
+        tip = "Tip: The number is odd.";
+    }
+    const answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: `Please guess the number between 1 to 10 (${tip})`,
+        }
+    ]);
+    console.log(`Your guess ${answer.userGuess} and system generated ${guessNum}`);
+    if (answer.userGuess === guessNum) {
+        score++;
+        console.log(`Congratulations your number is correct. Your score is: ${score}.`);
+    }
+    else {
+        console.log(`wrong guess number. Your score is ${score} Better try next time.`);
+    }
+}
